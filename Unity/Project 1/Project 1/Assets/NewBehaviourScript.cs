1976deda4696371b348fe1c34c04f2495fa48f34
Alea <alea.e.nelson@gmail.com>
2023-09-26 18:53:57 -0600

    Started Scripting Project 1
    Added some basic scripting for Project 1, more to follow.

---
+++ Unity/Project 1/Project 1/Assets/NewBehaviourScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	float moveSpeed = 5f;
+  	float jumpForce = 5f;
+  	float gravity = 9.81f;
+  	
+	CharacterController controller;
+	Vector3 moveDirection;
+	bool isJumping;
+	
+	void Start()
+	{
+	GetComponent(CharacterController)
+	{
+		Update
+		{
+		Input.GetAxis("Horizontal");
+		Input.GetAxis("Vertical");
+		
+		float horizontalInput = "Horizontal";
+		float verticalInput = "Vertical";
+		
+		Debug.Log("Horizontal");
+		Debug.Log("Vertical");
+		}
+	}
+	}
+}
