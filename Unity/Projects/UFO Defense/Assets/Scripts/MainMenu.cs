900802cb242f1702d134d4018f95283330a79400
Alea <alea.e.nelson@gmail.com>
2023-10-16 14:15:25 -0600

    Setting up TMP stuff

---
+++ Unity/Projects/UFO Defense/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
