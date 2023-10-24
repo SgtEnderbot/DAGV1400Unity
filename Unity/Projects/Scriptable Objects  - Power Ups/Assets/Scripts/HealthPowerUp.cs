49b049f62d2e922b6d665a8529b7144cf185b5a3
Alea <alea.e.nelson@gmail.com>
2023-10-24 12:14:38 -0600

    Changes
    Small changes, will likely not revisit this because of the realization that I'm supposed to keep working on the other project.

--- Unity/Projects/Scriptable Objects  - Power Ups/Assets/Scripts/HealthPowerUp.cs
+++ Unity/Projects/Scriptable Objects  - Power Ups/Assets/Scripts/HealthPowerUp.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class HealthPowerUp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private FloatData healthValue;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            ApplyHealth(other.gameObject);
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ApplyHealth(GameObject player)
     {
-        
+        HealthController healthController = player.GetComponent<HealthController>();
+
+        if (healthController != null)
+        {
+            healthController.AddHealth(healthValue.Value);
+        }
     }
+
 }
