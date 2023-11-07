04bc357761d7ae51a058fe68020190752f49b406
Alea <alea.e.nelson@gmail.com>
2023-11-07 16:14:54 -0700

    Color Match UI Progress 2
    Made changes to some scripts, added new scripts, and made it so the score updates properly.

--- Unity/Projects/2D Color Matching/Assets/Scripts/IntData.cs
+++ Unity/Projects/2D Color Matching/Assets/Scripts/IntData.cs
@@ -6,6 +6,11 @@ public class IntData : ScriptableObject
 {
     public int value;
 
+    public void SetValue(int num)
+    {
+        value = num;
+    }
+
     public void UpdateValue (int num)
     {
         value += num;
